6ac8576a74a621bb1384e0ddb49bbcaa8f191582
Cameron Upright <cameron.upright@gmail.com>
2015-10-08 00:34:17 -0700

    Moved async writing of messages to a helper method

--- src/Util/RLGlue/RLGlue++.h
+++ src/Util/RLGlue/RLGlue++.h
@@ -10,7 +10,7 @@
 #include <iostream>
 
 
-#include "../Proto/RLGlue.pb.h"
+#include "../../Proto/RLGlue.pb.h"
 
 using namespace std;
 
@@ -37,6 +37,99 @@ namespace RLGlue {
 		msg.ParseFromString(msgData);
 
 		return msg;
+
+	}
+
+
+	template <typename WriteHandler>
+	class AsyncWriteMessage :
+		public boost::enable_shared_from_this<AsyncWriteMessage<WriteHandler> >,
+		public boost::noncopyable {
+ 
+	public:
+
+		typedef boost::shared_ptr<AsyncWriteMessage<WriteHandler> > pointer;
+
+		static pointer create(boost::asio::ip::tcp::socket &socket,
+													BOOST_ASIO_MOVE_ARG(WriteHandler) handler) {
+
+			pointer ptr = pointer(new AsyncWriteMessage<WriteHandler>(socket, std::move(handler)));
+
+			return ptr;
+		}
+
+
+		void start(const ::google::protobuf::Message &msg) {
+
+			// Serialize the response
+			std::shared_ptr<std::string> messageWriteBuffer(new std::string());
+			msg.SerializeToString(messageWriteBuffer.get());
+
+			// Setup the header (size)
+			std::shared_ptr<std::vector<size_t> > headerWriteBuffer(new std::vector<size_t>({messageWriteBuffer->size()}));
+
+			// Write the header size first
+			boost::asio::async_write(socket_, boost::asio::buffer(*headerWriteBuffer),
+															 boost::bind(&AsyncWriteMessage<WriteHandler>::handleWriteResponseHeader, this->shared_from_this(),
+																					 messageWriteBuffer,
+																					 headerWriteBuffer,
+																					 boost::asio::placeholders::error,
+																					 boost::asio::placeholders::bytes_transferred));
+
+		}
+
+	private:
+
+		// Serializes the message, then starts an async write
+		AsyncWriteMessage(boost::asio::ip::tcp::socket &socket,
+											BOOST_ASIO_MOVE_ARG(WriteHandler) handler) :
+			socket_(socket), handler_(handler) {}
+
+
+
+		void handleWriteResponseHeader(std::shared_ptr<std::string> &messageWriteBuffer,
+																	 std::shared_ptr<std::vector<size_t> > &headerWriteBuffer,
+																	 const boost::system::error_code& error, size_t num_bytes) {
+					
+
+			if (error)
+				return;
+
+			// Setup the body buffer
+			std::shared_ptr<vector<char> > bodyWriteBuffer(new std::vector<char>(messageWriteBuffer->begin(), messageWriteBuffer->end()));
+
+			// Write the body, then when done read the next command
+			boost::asio::async_write(socket_, boost::asio::buffer(*bodyWriteBuffer),
+															 boost::bind(&AsyncWriteMessage<WriteHandler>::handleWriteResponseBody, this->shared_from_this(),
+																					 bodyWriteBuffer,
+																					 boost::asio::placeholders::error,
+																					 boost::asio::placeholders::bytes_transferred));
+		}
+
+
+		void handleWriteResponseBody(std::shared_ptr<std::vector<char> > &bodyWriteBuffer,
+																 const boost::system::error_code& error, size_t num_bytes) {
+
+			handler_(error, num_bytes);
+
+		}
+
+
+		boost::asio::ip::tcp::socket &socket_;	
+
+		WriteHandler handler_;
+
+
+	};
+
+
+
+	template <typename WriteHandler>
+	void asyncWriteMessage(boost::asio::ip::tcp::socket &socket,
+												 const ::google::protobuf::Message &msg,
+												 BOOST_ASIO_MOVE_ARG(WriteHandler) handler) {
+
+		AsyncWriteMessage<WriteHandler>::create(socket, std::move(handler))->start(msg);
 
 	}
 
@@ -125,20 +218,10 @@ namespace RLGlue {
 					// Step the environment
 					RewardStateTerminal rst = env_.step();
 
-					// Serialize the response
-					std::shared_ptr<std::string> messageWriteBuffer(new std::string());
-					rst.SerializeToString(messageWriteBuffer.get());
-
-					// Setup the header (size)
-					std::shared_ptr<std::vector<size_t> > headerWriteBuffer(new std::vector<size_t>({messageWriteBuffer->size()}));
-
-					// Write the header size first
-					boost::asio::async_write(socket_, boost::asio::buffer(*headerWriteBuffer),
-																	 boost::bind(&EnvConnection::handleWriteResponseHeader, shared_from_this(),
-																							 messageWriteBuffer,
-																							 headerWriteBuffer,
-																							 boost::asio::placeholders::error,
-																							 boost::asio::placeholders::bytes_transferred));
+					asyncWriteMessage(socket_, rst, 
+						boost::bind(&EnvConnection::handleWriteResponse, shared_from_this(),
+												boost::asio::placeholders::error,
+												boost::asio::placeholders::bytes_transferred));
 
 					break;
 				}
@@ -148,6 +231,34 @@ namespace RLGlue {
 
 			}
 
+		}
+
+
+
+		void handleWriteResponse(const boost::system::error_code& error, size_t num_bytes) {
+			readCommand();
+		}
+
+
+		/*
+		void asyncWriteMessage(boost::asio::ip::tcp::socket &socket, const ::google::protobuf::Message &msg) {
+		
+			// Serialize the response
+			std::shared_ptr<std::string> messageWriteBuffer(new std::string());
+			msg.SerializeToString(messageWriteBuffer.get());
+
+			// Setup the header (size)
+			std::shared_ptr<std::vector<size_t> > headerWriteBuffer(new std::vector<size_t>({messageWriteBuffer->size()}));
+
+			// Write the header size first
+			boost::asio::async_write(socket_, boost::asio::buffer(*headerWriteBuffer),
+															 boost::bind(&EnvConnection::handleWriteResponseHeader, shared_from_this(),
+																					 messageWriteBuffer,
+																					 headerWriteBuffer,
+																					 boost::asio::placeholders::error,
+																					 boost::asio::placeholders::bytes_transferred));
+
+		
 		}
 
 
@@ -176,7 +287,7 @@ namespace RLGlue {
 			readCommand();
 
 		}
-
+		*/
 
 
 		Env &env_;
